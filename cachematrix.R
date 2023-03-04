ea9849368f21ef3769f5e67680ece81fb0caddd4
JC <enai.jeanette@gmail.com>
2023-03-04 10:59:49 +0800

    update with solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Build the inverse matrix and return its data to the parent environment
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeMatrix <- function(x = matrix()) {
+  inversex <- NULL
+  set <- function(y) {
+    x <<- y
+    inversex <<-NULL
+  }
+  get <- function() x
+  setinverse <-function(solve) inversex <<- solve
+  getinverse <- function() inversex
+  list (set =set, get=get,
+        setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Code block to retrieve Matrix and populate
 
-cacheSolve <- function(x, ...) {
+cacheinverse <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  inversex <- x$getinverse()
+    if(!is.null(inversex)) {
+      message("getting cached data")
+      return(inversex)
+    }
+    data <- x$get()
+    inversex <- solve(data, ...)
+    x$setinverse(inversex)
+    inversex
+  }
+
